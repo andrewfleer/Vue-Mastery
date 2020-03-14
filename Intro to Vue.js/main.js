5e09d68cf4d42bfff62369635fab02186e67b4cd
Andrew <andrew.fleer@gmail.com>
2020-03-13 22:40:23 -0500

    Conditional Rendering!

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -4,6 +4,9 @@ var app = new Vue({
         product: 'Socks',  // The data for this element, to fill in anything with {{ }}
         description: 'A pair of warm, fuzzy socks',
         image: './assets/vmSocks-green-onWhite.jpg', // The data can also fill in a v-bind tag
-        link: 'http://www.vuemastery.com'
+        link: 'http://www.vuemastery.com',
+        inStock: true, // You can use booleans for v-if / v-else conditional rendering.
+        onSale: true,
+        inventory: 100 // Or you can use v-if for more conditional logic.
     }
 })
