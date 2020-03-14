7da31ee302146dee01466fb0dc27c7acd1001592
Andrew <andrew.fleer@gmail.com>
2020-03-13 22:29:33 -0500

    Learned about attribute binding.

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',  // This tells the JS to go to an element with the ID "#app".
     data: {
         product: 'Socks',  // The data for this element, to fill in anything with {{ }}
-        description: 'A pair of warm, fuzzy socks'
+        description: 'A pair of warm, fuzzy socks',
+        image: './assets/vmSocks-green-onWhite.jpg', // The data can also fill in a v-bind tag
+        link: 'http://www.vuemastery.com'
     }
 })
