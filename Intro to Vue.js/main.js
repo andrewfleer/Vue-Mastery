909efca0231d3f90ce8e7ddcc1339c97c996cfa1
Andrew <andrew.fleer@gmail.com>
2020-03-13 23:09:05 -0500

    Learned about event handling.

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -3,7 +3,7 @@ var app = new Vue({
     data: {
         product: 'Socks',  // The data for this element, to fill in anything with {{ }}
         description: 'A pair of warm, fuzzy socks',
-        image: './assets/vmSocks-green-onWhite.jpg', // The data can also fill in a v-bind tag
+        image: './assets/vmSocks-green.jpg', // The data can also fill in a v-bind tag
         link: 'http://www.vuemastery.com',
         inStock: true, // You can use booleans for v-if / v-else conditional rendering.
         onSale: true,
@@ -13,12 +13,26 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: './assets/vmSocks-green.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: './assets/VmSocks-blue.jpg'
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {  // These are functions that can be loaded through Vue
+        addToCart: function () {
+            this.cart += 1  // "this" references the cart within this element.
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        updateProduct(variantImage) { // You can use a shorthand instead of declaring anonymous functions. However, not all browsers support this.
+            this.image = variantImage
+        }
     }
 })
