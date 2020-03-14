6947fc8aa52419173bf7b04618e12e6b3c43c78d
Andrew <andrew.fleer@gmail.com>
2020-03-13 22:15:53 -0500

    Added comments so I can remember later.

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -1,7 +1,7 @@
 var app = new Vue({
-    el: '#app',
+    el: '#app',  // This tells the JS to go to an element with the ID "#app".
     data: {
-        product: 'Socks',
+        product: 'Socks',  // The data for this element, to fill in anything with {{ }}
         description: 'A pair of warm, fuzzy socks'
     }
 })
