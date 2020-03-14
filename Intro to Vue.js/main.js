478295eec77eadef237c0cc2dbb890c2debc5b4c
Andrew <andrew.fleer@gmail.com>
2020-03-14 00:18:43 -0500

    Moved the product data to components. This allows for reusability and modularization.

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -1,7 +1,73 @@
-var app = new Vue({
-    el: '#app',  // This tells the JS to go to an element with the ID "#app".
-    data: {
-        brand: 'Vue Mastery',
+Vue.component('product',{
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+        <div class="product">
+        <!-- The {{ }} are expressions. They pull settings from the javascript.-->
+        <div class="product-image">
+            <img v-bind:src="image" v-bind:alt="description" />  <!-- v-bind: dynamically binds an attribute to an expression -->
+                                                                <!-- a shorthand method is to just use the colon. <img :src="image"> -->
+        </div>
+
+        <div class="product-info">
+            <h1>{{ title }}</h1> <!-- This is now a computed property -->
+            <p v-if="inventory > 10">In Stock</p>  <!-- Conditional Rendering -->
+            <p v-else-if="inventory<= 10 && inventory > 0">Almost sold out!</p>
+            <p v-else
+            :class="{ lineThrough : !inStock}">Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+            <p >{{ sale }}</p>
+            <p v-show="inStock">Buy Today!</p> <!-- v-show also conditionally shows thing. v-if adds and removes elements from the DOM.-->
+                                            <!-- v-show just hides/unhides through CSS. Better for performance.-->
+            <p>{{ description }}</p> <!-- This is a plain, boring, regular property-->
+
+            <p>
+            <product-details :details="details"></product-details>                   
+            </p>
+
+            <p>
+                <strong>Sizes</strong>
+                <ul>
+                    <li v-for="size in sizes">{{ size }}</li>
+                </ul>
+            </p>
+
+            <strong>Colors</strong>
+            <!-- The style binding let's us change CSS styles dynamically. 
+                Here we're changing the background color of the div, based on the variant configs-->
+            <div v-for="(variant, index) in variants" 
+                :key="variant.variantId"
+                class="color-box" 
+                :style="{ backgroundColor: variant.variantColor }"
+                @mouseover="updateProduct(index)"> <!-- How to render a list of object. Always a good idea to use the key binding. -->
+                
+            </div>
+
+            <div>
+                <a v-bind:href="link">Link to Vue Mastery!</a>
+            </div>
+
+            <div>
+
+                <!-- Here we did class binding. This is a way to dynamically add a CSS class to an element.-->
+                <button v-on:click="addToCart" 
+                        :disabled="!inStock"
+                        :class="{ disabledButton: !inStock }">Add to Cart</button> <!-- This is a cool way for Vue to read from the DOM-->
+                <button @click="removeFromCart">Remove from Cart</button> <!-- the @ is shorthand for v-on -->
+
+                <div class="cart">
+                    <p>Cart({{cart}})</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `,
+    data() {
+        return { brand: 'Vue Mastery',
         product: 'Socks',  // The data for this element, to fill in anything with {{ }}
         description: 'A pair of warm, fuzzy socks',
         selectedVariant: 0,
@@ -25,6 +91,7 @@ var app = new Vue({
             }
         ],
         cart: 0
+    }
     },
     methods: {  // These are functions that can be loaded through Vue
         addToCart: function () {
@@ -58,6 +125,33 @@ var app = new Vue({
             }
             
             return this.brand + ' ' + this.product + ' are not on sale'
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return 2.99
         }
     }
+})
+
+Vue.component('product-details', {
+    props: {
+      details: {
+        type: Array,
+        required: true
+      }
+    },
+    template: `
+      <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>
+    `
+  })
+
+var app = new Vue({
+    el: '#app',  // This tells the JS to go to an element with the ID "#app".
+    data: {
+        premium: true
+    }   
 })
